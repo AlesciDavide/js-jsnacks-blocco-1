9128c5fa8463b1ea71c781ad8f6c80c2731fa479
AlesciDavide <davi.alesci@email.com>
2024-05-13 12:35:22 +0200

    dichiarazione i dentro il for

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -1,18 +1,14 @@
 /* Crea un array vuoto.
- hiedi per 6 volte all'utente di inserire un numero,
+hiedi per 6 volte all'utente di inserire un numero,
  se è dispari inseriscilo nell'array. */
 
 
 let listaNumeri = [];
-let i = 0;
 
-for(i = 0; i < 6; i++){
-
+for(let i = 0; i < 6; i++){
     let numeroInserito = Number.parseInt(prompt('isnerisci il numero' + (i+1)));
-
     if( numeroInserito % 2 == 1){
         listaNumeri.push(numeroInserito);
-
     };
 }
 console.log(listaNumeri);
