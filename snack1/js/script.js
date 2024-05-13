f743778347a16d0cdb985ce9407d7ecd75cae55b
AlesciDavide <davi.alesci@email.com>
2024-05-13 11:57:15 +0200

    aggiunto condizione numeri uguali dopo lc

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,12 +1,17 @@
-/* L'utente inserisce due numeri in successione, con due prompt.
+/* Snack 1. L'utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
 
-let numeroUno = Number.parseInt(prompt('Inserisci il primo numero'));
-let numeroDue = Number.parseInt(prompt('Inserisci il primo numero'));
+let numeroUno = Number.parseInt(prompt('Inserisci il primo numero'), 10);
+let numeroDue = Number.parseInt(prompt('Inserisci il secondo numero'), 10);
 
-if(numeroUno > numeroDue){
+if(numeroUno === numeroDue){
+    console.log(numeroUno , numeroDue);
+}else if(numeroUno > numeroDue){
     console.log(numeroUno);
 }else{
     console.log(numeroDue);
 }
+
+
+/* aggiunto condizione numeri uguali */
