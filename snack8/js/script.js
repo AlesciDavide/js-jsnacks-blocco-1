3c413646e24c536579f4d704ad9f76ce5ec35e5d
AlesciDavide <davi.alesci@email.com>
2024-05-14 11:42:40 +0200

    aggiunti numeri e spazi tra i numeri

--- snack8/js/script.js
+++ snack8/js/script.js
@@ -1,7 +1,7 @@
 /* Snack8 
 Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari. */
 
-const listaNumeri = [5,6,8,5,1,85,2,4,85,2,6];
+const listaNumeri = [5,6,8,5,1,85,2,4,85,2,6,7,5,3,21,57,100];
 
 const sectionElement = document.querySelector('section#myContainer');
 
@@ -16,12 +16,12 @@ divElementTre .classList.add('bg_yellow');
 
 for(let i = 0; i < listaNumeri.length; i++){
     if(listaNumeri[i] % 5 == 0){
-        divElementTre.append(listaNumeri[i]);
+        divElementTre.append(listaNumeri[i] + ' ');
 
     }else if (listaNumeri[i] % 2 == 1 ){
-        divElement.append(listaNumeri[i]);
+        divElement.append(listaNumeri[i] + ' ');
     }else{
-        divElementDue.append(listaNumeri[i]);
+        divElementDue.append(listaNumeri[i] + ' ');
 
     }
     sectionElement.appendChild(divElement);
